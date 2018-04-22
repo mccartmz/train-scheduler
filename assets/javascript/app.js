60f58185d37268b221685a5e984bd57cf976b101
Matt McCarthy <34220681+mccartmz@users.noreply.github.com>
2018-04-21 20:58:41 -0400

    Update app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,24 @@
-// Firebase data
-var config = {
-    apiKey: "AIzaSyCEkaPMTXIXWUMpktD08z7ZR5TVh6cNeXw",
-    authDomain: "project-1-25453.firebaseapp.com",
-    databaseURL: "https://project-1-25453.firebaseio.com",
-    projectId: "project-1-25453",
-    storageBucket: "project-1-25453.appspot.com",
-    messagingSenderId: "736620026938"
-};
-firebase.initializeApp(config);
+  var config = {
+    apiKey: "AIzaSyBsYR6OXOHsmMFQ0EqLhUtw3N1Gf8iDRB8",
+    authDomain: "train-project-2010e.firebaseapp.com",
+    databaseURL: "https://train-project-2010e.firebaseio.com",
+    projectId: "train-project-2010e",
+    storageBucket: "train-project-2010e.appspot.com",
+    messagingSenderId: "725400376763"
+  };
+    firebase.initializeApp(config);
 
 var database = firebase.database();
 
-$("#addTrain").on("click", function (event) {
+$("#addTrainButton").on("click", function (event) {
     event.preventDefault();
 
     // Gets user data
     var name = $("#trainName").val().trim();
     var destination = $("#destination").val().trim();
-    var time = moment($("#firstTrainTime").val().trim(), "HH:MM").format("X");
+    var time = moment($("#firstTrainTime").val().trim(), "HH:mm").subtract(10,"days").format("X");
     var frequency = $("#frequency").val().trim();
+
 
 
     var addTrain = {
@@ -31,12 +31,15 @@ $("#addTrain").on("click", function (event) {
 
     database.ref().push(addTrain);
 
+   alert("New train has been added")
+
     // Clear boxes
     $("#trainName").val("");
     $("#destination").val("");
     $("#firstTrainTime").val("");
     $("#frequency").val("");
 
+    return false;
 });
 
 // User adds an entry
@@ -47,22 +50,17 @@ database.ref().on("child_added", function (snapshot) {
     var frequency = snapshot.val().frequency;
 
 
-    var time = moment.unix(time).format("HH:MM")
-    var formattedTime = moment(time, "HH:MM").subtract(1, "years");
-   
+  
+  //  var formattedTime = moment(time, "HH:MM").subtract(1, "years");
 
-    var diffTime = moment().diff(moment(formattedTime), "minutes");
-    var remainder = (diffTime % frequency);
+    var remainder = moment().diff(moment.unix(time), "minutes")%frequency;
     var minutesAway = frequency - remainder;
-    console.log(minutesAway);
-
-
-    var nextTrainTime = moment().add(minutesAway, "minutes");
+    var arrivalTime = moment().add(minutesAway,"m").format("LT");
 
 
 
-    // Display data in the table
+    //Display data in the table
     $("#trainTable > tbody").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" +
-        frequency + "</td><td>" + nextTrain + "</td><td>" + minutesAway + "</td><td>");
+        frequency + "</td><td>" + arrivalTime + "</td><td>" + minutesAway + "</td><td>");
 
 });
